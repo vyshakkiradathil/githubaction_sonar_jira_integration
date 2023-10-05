b6716aeff08039ae35a1b535b463fb6abca3f327
jayseth <46541147+jayseth@users.noreply.github.com>
2023-10-05 16:10:45 +0530

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -4,8 +4,9 @@ public class Program
 {
   public static void Main()
   {
-    String Username = "admin";
-    String Password = "admin";
+    Console.WriteLine("Hello world, this is Jay");
+    Console.WriteLine("Hello world, this is Jay");
+    Console.WriteLine("Hello world, this is Jay");
     Console.WriteLine("Hello world, this is Jay");
   }
 }
