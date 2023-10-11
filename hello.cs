f5aa2c7c1787d75528bad1204390820c4d48d381
jayseth <46541147+jayseth@users.noreply.github.com>
2023-10-12 01:44:48 +0530

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -7,7 +7,7 @@ public class Program
   public static void Main()
   {
     ExecuteQuery("Jay");
-    Console.WriteLine("Hello world, this is the new code");
+    Console.WriteLine("Hello world, this is the newer code");
     System.String username = "testuser";
     System.String password = "testpass";
     CallApi(username, apiKey);
